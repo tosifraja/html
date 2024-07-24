5884cb033971d979740b2d61079df89fe1064911
tosifraja <tosikreja786786@gmail.com>
2024-07-24 17:35:36 +0545

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  strings: ["Student", "Hacker", "Programmer", "Physics Enthutiast", "Dreamer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
